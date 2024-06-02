be72692ad2ddb2c1d087041c69e5741f578b6526
0x2170 <151378415+DLLenjoyer@users.noreply.github.com>
2024-06-02 22:18:56 +0200

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,19 @@
+package config
 
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+func SetupDatabase() (*gorm.DB, error) {
+	dsn := "host=localhost user=user password=password dbname=mydb port=5432 sllmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Подключение к базе данных успешно!")
+	return db, nil
+}
