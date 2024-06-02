2881d84435dd12aaf4167d84e86862cb712332d3
0x2170 <151378415+DLLenjoyer@users.noreply.github.com>
2024-06-02 22:24:06 +0200

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,17 @@
+package routes
 
+import (
+	"github.com/DLLenjoyer/AdminApp-with-api/controllers"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/users", func(c *gin.Context) {
+			controllers.GetAllUsers(c, db)
+		})
+	}
+}
